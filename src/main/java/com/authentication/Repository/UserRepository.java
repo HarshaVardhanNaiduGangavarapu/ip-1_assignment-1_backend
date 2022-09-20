d056cc6f9ed4e4edd13a92bc02a666f4df39e2bf
Ghavan <ghavan@gmail.com>
2022-09-20 03:25:09 -0400

    Created API to update user details including image

--- src/main/java/com/authentication/Repository/UserRepository.java
+++ src/main/java/com/authentication/Repository/UserRepository.java
@@ -1,6 +1,11 @@
 package com.authentication.Repository;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.authentication.Entity.UserMstEntity;
 
@@ -9,4 +14,12 @@ import com.authentication.Entity.UserMstEntity;
 public interface UserRepository extends CrudRepository<UserMstEntity, Integer>{
 
 	UserMstEntity findByUserEmail(String mobileNo);
+	
+	@Transactional
+	@Modifying
+	@Query(value = "update user_mst u set u.name= :name ,u.email = :email ,"
+			+ "u.bio = :bio,u.mobile_no = :mobileNo,u.photo = :photo where u.email = :email", nativeQuery = true)
+	int updateUserRestByEmail(@Param("name") String userName, @Param("email") String userEmail,
+			@Param("bio") String userBio, @Param("mobileNo") String mobileNo,
+			@Param("photo") byte[] imageFile);
 }
